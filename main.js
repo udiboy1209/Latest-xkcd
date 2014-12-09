f45a992fa674b435e1ab8ab762871907948313aa
Meet Udeshi <mudeshi1209@gmail.com>
2014-12-09 20:32:17 +0530

    Add random loading button

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
+var num = location.search.replace("?","");
+console.log(num)
+var fileref=document.createElement('script')
+fileref.setAttribute("type","text/javascript")
+fileref.setAttribute("src", "https://dynamic.xkcd.com/api-0/jsonp/comic/"+num+"?callback=storeComic")
 
-var comicData;
-    
+function loadRandom(){
+    num = Math.floor(Math.random()*parseInt(num))
+    window.open( "xkcd.htm?"+num,"_self")
+} 
+
 function storeComic(data){
-    comicData=data;
+	document.getElementById("title").innerHTML = data.title;
+	document.getElementById("image").src = data.img;
+	document.getElementById("image").title = data.alt;
+    num = data.num
 }
 
-
 document.addEventListener('DOMContentLoaded', function () {
-	document.getElementById("title").innerHTML = comicData.title;
-	document.getElementById("image").src = comicData.img;
-	document.getElementById("image").title = comicData.alt;
-});
+        document.getElementById('random').addEventListener('click', loadRandom);
+        document.getElementsByTagName("body")[0].appendChild(fileref)
+})
