622c2a134f35b9015d49f895464c1c15ae6ef9ec
Meet Udeshi <mudeshi1209@gmail.com>
2015-05-10 01:11:53 +0530

    Improve random function
    Better hack to the one suggested in  #2
    We keep passing max_num parameter which is the no.
    of current existing comics, and randomly selects one from
    them

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-var num = location.search.replace("?","");
-console.log(num)
+var params = location.search.replace("?","").split("&");
+var num = params[0];
+var max_num = (params.length>1?params[1]:num);
+
+console.log(num);
+console.log(max_num);
+
 var fileref=document.createElement('script')
 fileref.setAttribute("type","text/javascript")
 fileref.setAttribute("src", "https://dynamic.xkcd.com/api-0/jsonp/comic/"+num+"?callback=storeComic")
 
 function loadRandom(){
-    num = Math.floor(Math.random()*parseInt(num))
-    window.open( "xkcd.htm?"+num,"_self")
+    num = Math.floor(Math.random()*parseInt(max_num))
+    window.open( "xkcd.htm?"+num+"&"+max_num,"_self")
 } 
 
 function storeComic(data){
 	document.getElementById("title").innerHTML = data.title;
 	document.getElementById("image").src = data.img;
 	document.getElementById("image").title = data.alt;
+    if(max_num=="") max_num=data.num
     num = data.num
 }
 
